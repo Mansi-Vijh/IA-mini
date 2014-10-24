38ca2153d9dfb825d733c8d900c7028ec7380ef7
Mansi Vijh <mansivijh94@gmail.com>
2014-10-24 16:42:42 +0530

    Open FD feature added

--- mini/mini/Main.Designer.cs
+++ mini/mini/Main.Designer.cs
@@ -28,20 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.Original = new Emgu.CV.UI.ImageBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Original)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog1";
+            // 
+            // Original
+            // 
+            this.Original.Location = new System.Drawing.Point(121, 152);
+            this.Original.Name = "Original";
+            this.Original.Size = new System.Drawing.Size(447, 553);
+            this.Original.TabIndex = 2;
+            this.Original.TabStop = false;
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(121, 62);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(120, 23);
+            this.btn_browse.TabIndex = 3;
+            this.btn_browse.Text = "Choose Image";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1012, 466);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1012, 750);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.Original);
+            this.Name = "Main";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Original)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private Emgu.CV.UI.ImageBox Original;
+        private System.Windows.Forms.Button btn_browse;
     }
 }
 
--- mini/mini/Main.cs
+++ mini/mini/Main.cs
@@ -7,13 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+
 namespace mini
 {
     public partial class Main : Form
-    {
+    {   
+        string Chosen_File = "";
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            openFD.Title = "Insert an Image";
+            openFD.FileName = "";
+            openFD.Filter = " TIFF Images|*.tif|JPEG Images|*.jpg|GIF Images|*.gif|BITMAPS|*.bmp|PNG Images|*.png|PICT Images|*.pct";
+            if (openFD.ShowDialog() == DialogResult.Cancel)
+            {
+                MessageBox.Show("Choose image to proceed");
+            }
+            else
+            {
+                Chosen_File = openFD.FileName;
+                Image<Gray, byte> img = new Image<Gray, byte>(Chosen_File);
+                Original.Image = img;
+            }
         }
     }
 }
